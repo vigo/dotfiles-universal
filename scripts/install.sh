80c76774fe3c041c862f20a76744c8975e792606
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2016-10-02 20:03:16 +0300

    fix: install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,9 +5,14 @@ set -e
 install_dotfiles() {
     echo "This script will link some files under your ${HOME} directory."
     echo
-    mv $HOME/.bashrc $HOME/bashrc_old > /dev/null 2>&1
-    mv $HOME/.profile $HOME/profile_old > /dev/null 2>&1
-    
+    if [[ -f "${HOME}/.profile" ]]; then
+        mv $HOME/.profile $HOME/.profile_old && echo "~/.profile found and moved as: ~/.profile_old"
+    fi
+
+    if [[ -f "${HOME}/.bashrc" ]]; then
+        mv $HOME/.bashrc $HOME/.bashrc_old && echo "~/.bashrc found and moved as: ~/.bashrc_old"
+    fi
+
     ln -s "${HOME}/Dotfiles/profile" "${HOME}/.profile" && echo "Linked: ~/.profile"
     ln -s "${HOME}/Dotfiles/rc/bashrc" "${HOME}/.bashrc" && echo "Linked: ~/.bashrc"
     ln -s "${HOME}/Dotfiles/rc/inputrc" "${HOME}/.inputrc" && echo "Linked: ~/.inputrc"
